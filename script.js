b03eac054e455451a63c892d77c0014fffe7f1e2
kgdee <kgdee7603@gmail.com>
2024-04-13 03:37:08 +0800

    commit

--- script.js
+++ script.js
@@ -10,22 +10,21 @@ async function updateCardsEl() {
     const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
     const repositories = await response.json();
     
-    repositories.forEach(async (repo) => {
-      if (repo.has_pages) {
-        const name = repo.name.replace(/-/g, ' ')
-        const iconUrl = `https://${username}.github.io/${repo.name}/favicon.png`
-        const icon = await checkImageUrl(iconUrl) ? iconUrl : "images/internet.png"
-        const pageUrl = `https://${username}.github.io/${repo.name}/`
-        cardsEl.innerHTML += `
-          <a href="${pageUrl}" target="_blank" class="card">
-            <img src=${icon} class="icon">
-            <p class="title">${name}</p>
-          </a>
-        `
-      } else {
-        // console.log(`GitHub Pages is not enabled for ${repo.full_name}`);
-      }
-    });
+    for (const repo of repositories) {
+      if (!repo.has_pages) continue
+
+      const name = repo.name.replace(/-/g, ' ')
+      const iconUrl = `https://${username}.github.io/${repo.name}/favicon.png`
+      const icon = await checkImageUrl(iconUrl) ? iconUrl : "images/internet.png"
+      const pageUrl = `https://${username}.github.io/${repo.name}/`
+      cardsEl.innerHTML += `
+        <a href="${pageUrl}" target="_blank" class="card">
+          <img src=${icon} class="icon">
+          <p class="title">${name}</p>
+        </a>
+      `
+    }
+
   } catch (error) {
     console.error('Error fetching repositories:', error);
   }
@@ -53,3 +52,13 @@ function checkImageUrl(url) {
 document.addEventListener("DOMContentLoaded", function() {
   updateCardsEl()
 })
+
+
+
+
+
+window.addEventListener("error", (event) => {
+  const error = `${event.type}: ${event.message}`
+  console.error(error)
+  alert(error)
+});
