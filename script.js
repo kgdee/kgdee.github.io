fa754dccfe5b9346b3d96d2b829c84b0e3c47773
kgdee <kgdee7603@gmail.com>
2025-04-13 02:32:46 -0400

    commit

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const storagePrefix = "github-home_"
+const storagePrefix = "github-home_";
 
-const cardsEl = document.querySelector(".cards")
+const cardsContainer = document.querySelector(".cards-container");
 
-const username = 'kgdee';
+const username = "kgdee";
 
-async function updateCardsEl() {
+document.addEventListener("DOMContentLoaded", function() {
+  updateCardsContainer()
+})
+
+async function updateCardsContainer() {
   try {
     const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
     const repositories = await response.json();
-    
-    for (const repo of repositories) {
-      if (!repo.has_pages) continue
-      console.log(repo.id)
 
-      const name = repo.name.replace(/-/g, ' ')
-      const iconUrl = `https://${username}.github.io/${repo.name}/favicon.png`
-      const icon = "images/internet.png"
-      const pageUrl = `https://${username}.github.io/${repo.name}/`
-      cardsEl.innerHTML += `
+    for (const repo of repositories) {
+      if (!repo.has_pages) continue;
+
+      const name = repo.name.replace(/-/g, " ");
+      const iconUrl = `https://${username}.github.io/${repo.name}/favicon.png`;
+      const icon = "card-icon.png";
+      const pageUrl = `https://${username}.github.io/${repo.name}/`;
+      cardsContainer.innerHTML += `
         <a href="${pageUrl}" class="card" data-repo-id="${repo.id}">
-          <img src=${icon} class="icon">
+          <div class="icon-wrapper">
+            <img src=${icon} class="icon">
+          </div>
           <p class="title">${name}</p>
         </a>
-      `
-      updateCardIcon(repo.id, iconUrl)
+      `;
+      updateCardIcon(repo.id, iconUrl);
     }
-
   } catch (error) {
-    console.error('Error fetching repositories:', error);
+    console.error("Error fetching repositories:", error);
   }
 }
 
@@ -35,12 +39,12 @@ function checkImageUrl(url) {
   return new Promise((resolve) => {
     const img = new Image();
 
-    img.onload = function() {
+    img.onload = function () {
       // Image loaded successfully
       resolve(true);
     };
 
-    img.onerror = function() {
+    img.onerror = function () {
       // Error loading image
       resolve(false);
     };
@@ -50,25 +54,16 @@ function checkImageUrl(url) {
 }
 
 async function updateCardIcon(repoId, iconUrl) {
+  const iconUrlIsValid = await checkImageUrl(iconUrl);
 
-  const iconUrlIsValid = await checkImageUrl(iconUrl)
-  
   if (iconUrlIsValid) {
-    const iconEl = document.querySelector(`.card[data-repo-id="${repoId}"] img`)
-    iconEl.src = iconUrl
+    const iconEl = document.querySelector(`.card[data-repo-id="${repoId}"] img`);
+    iconEl.src = iconUrl;
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  updateCardsEl()
-})
-
-
-
-
-
 window.addEventListener("error", (event) => {
-  const error = `${event.type}: ${event.message}`
-  console.error(error)
-  alert(error)
+  const error = `${event.type}: ${event.message}`;
+  console.error(error);
+  alert(error);
 });
